de2f730dc1813611b07429f5fdaf7aa146fba308
OtegaClinton <oghenetegaokotie@gmail.com>
2024-08-07 12:10:23 +0100

    second push

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+require("dotenv").config();
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -8,3 +10,17 @@ const Port = process.env.PORT || 5050
 app.listen(Port,()=>{
     console.log(`server is listening to PORT: ${Port}.`)
 });
+
+
+
+
+
+const Uri = process.env.DATABASE_URI;
+
+mongoose.connect(Uri)
+.then(()=>{
+    console.log(`Database connected successfully.`)
+})
+.catch((error)=>{
+    console.log(`Error connecting to database.`, error)
+}); 
